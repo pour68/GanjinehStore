a9209fd5ce8ba484509e15ac521291dc3377cd03
pour68 <pour68@outlook.com>
2021-06-29 12:52:16 +0430

    Add generic services to application.

---
+++ GanjinehStore/Services/Interfaces/IService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace GanjinehStore.Services.Interfaces
+{
+    public interface IService<T> where T: class
+    {
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        T Add(T tEntity);
+        T Remove(int id);
+        T Update(int id, T tEntity);
+    }
+}
---
+++ GanjinehStore/Services/Service.cs
@@ -0,0 +1,84 @@
+﻿using GanjinehStore.Data;
+using GanjinehStore.Services.Interfaces;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GanjinehStore.Services
+{
+    public class Service<T> : IService<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<Service<T>> _logger;
+
+        public Service(ApplicationDbContext context, ILogger<Service<T>> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        public IEnumerable<T> GetAll() => _context.Set<T>().ToList();
+
+        public T GetById(int id) => _context.Set<T>().Find(id);
+
+        public T Add(T tEntity)
+        {
+            try
+            {
+                _context.Set<T>().Add(tEntity);
+                _context.SaveChanges();
+
+                return tEntity;
+            }
+            catch (Exception ex)
+            {
+                LogError(ex.StackTrace, ex.Message);
+
+                return null;
+            }
+        }
+
+        public T Remove(int id)
+        {
+            try
+            {
+                var tEntity = GetById(id);
+
+                _context.Set<T>().Remove(tEntity);
+                _context.SaveChanges();
+
+                return tEntity;
+            }
+            catch (Exception ex)
+            {
+                LogError(ex.StackTrace, ex.Message);
+
+                return null;
+            }
+        }
+
+        public T Update(int id, T tEntity)
+        {
+            try
+            {
+                _context.Set<T>().Update(tEntity);
+                _context.SaveChanges();
+
+                return tEntity;
+            }
+            catch (Exception ex)
+            {
+                LogError(ex.StackTrace, ex.Message);
+
+                return null;
+            }
+        }
+
+        // helpers
+        public void LogError(string stackTrace, string message) 
+        {
+            _logger.LogError($"{stackTrace} - {message}");
+        }
+    }
+}
